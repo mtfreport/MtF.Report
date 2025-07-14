b03237f1dc7dbbcbf68819da0bb1012c33b5b7be
kazukokawagawa <2580099704@qq.com>
2025-07-14 14:00:45 +0800

    update:sans

--- example/docs/.vitepress/theme/index.ts
+++ example/docs/.vitepress/theme/index.ts
@@ -1,10 +1,10 @@
 // docs/.vitepress/theme/index.ts
 // https://vitepress.dev/guide/custom-theme
 import PtjsTheme from '@project-trans/vitepress-theme-project-trans/theme'
-
+import DefaultTheme from 'vitepress/theme-without-fonts'
 import 'uno.css'
 import './style.css'
 
 export default {
-  extends: PtjsTheme,
+  extends: PtjsTheme,DefaultTheme
 }
